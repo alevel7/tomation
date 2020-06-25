4d7e425a5f55cc111dd01d7224758ff4a47f53c1
kazeem taiwo <alevel7@gmail.com>
2020-06-25 22:42:02 +0100

    pricing page added

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,10 +1,31 @@
-function toggleDropDown() {
-    var ul = document.querySelector('header>div:nth-child(2)');
-    if (ul.classList.contains('hidden')) {
-      ul.classList.remove('hidden');
-      ul.classList.add('block')
-    } else {
-      ul.classList.remove('block');
-      ul.classList.add('hidden')
+$(document).ready(function () {
+  var state = false
+  var substate = false
+  $('header > div > div > button').click(function () {
+    if (state == false) {
+      $('header > div > ul').slideDown(500)
+      state = true;
     }
-  }
+    else {
+      $('header > div > ul').slideUp()
+      state = false
+    }
+  })
+
+  $('.dropdown').mouseenter(function(event){
+    $(event.currentTarget).children('ul').slideDown()
+  })
+  $('.dropdown').mouseleave(function(event){
+    $(event.currentTarget).children('ul').slideUp()
+  })
+
+  $('.dropdown > span').click(function(event){
+    if(substate == false){
+          $(event.currentTarget).siblings().slideDown()
+          substate = true;
+    } else {
+       $(event.currentTarget).siblings().slideUp()
+       substate = false;
+    }
+  })
+})
